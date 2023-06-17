cd344cd96cdfa1947d69940475d9a06d53041060
Marius Bause <mariusbause@gmx.de>
2023-06-18 00:02:50 +0200

    python 3.10 added

--- run_test.sh
+++ run_test.sh
@@ -1,8 +1,15 @@
 start=$(date +%s%N)
-python src/add.py 
+$HOME/.pyenv/versions/3.10.12/bin/python src/add.py 
 end=$(date +%s%N)
 runtime=$((end-start))
-echo 'Python runtime for add:' $(echo "scale=3;($runtime)/(1*10^06)" | bc) 'ms'
+echo 'Python 3.10 runtime for add:' $(echo "scale=3;($runtime)/(1*10^06)" | bc) 'ms'
+
+start=$(date +%s%N)
+$HOME/.pyenv/versions/3.11.4/bin/python src/add.py 
+end=$(date +%s%N)
+runtime=$((end-start))
+echo 'Python 3.11 runtime for add:' $(echo "scale=3;($runtime)/(1*10^06)" | bc) 'ms'
+
 
 start=$(date +%s%N)
 julia src/add.jl  
